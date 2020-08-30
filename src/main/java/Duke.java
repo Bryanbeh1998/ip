d3f0bf02df41327658504713f977d53fbfb65833
DESKTOP-E1K9FDF\bryan <bryanthebeh98@gmail.com>
2020-08-30 12:15:08 +0800

    Level3.MarkAsDone

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 
+import java.net.Inet4Address;
 import java.util.Scanner;
 import java.util.ArrayList;
+
 
 
 public class Duke {
@@ -15,27 +17,44 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can i do for you?\n");
 
-
-
         Scanner input = new Scanner(System.in);
         ArrayList<String> list = new ArrayList<>();
         int i = 1;
+
         while (true){
             String words = input.nextLine();
-            if (words.equals("bye")){
+            Task t = new Task(words);
+
+            if (t.description.equals("bye")){
                 System.out.println("Bye! Hope to see you again");
                 break;
             }
-            if (words.equals("list")) {
-                for (String s : list) {
 
-                    System.out.println(i + "." + s );
-                    i++;
+            else if (t.description.equals("list")) {
+
+                System.out.println("Here are the tasks in your list:");
+                for (String s : list) {
+                    System.out.println(s);
                 }
             }
+            else if(t.description.contains("done")){
+                t.setAsDone();
+                System.out.println("Nice! I've marked this task as done");
+                String taskNumberString = t.description.replaceAll("[^0-9]",""); //extract the int
+                int taskNumber = Integer.parseInt(taskNumberString);    //convert string to int
+
+                String doneTask = list.get(taskNumber-1);   //extract the task that is done
+                int dividerPosition = doneTask.indexOf("\u2718");
+                String justTask = doneTask.substring(dividerPosition + 1,doneTask.length()); //remove the number and symbol
+
+                System.out.println(taskNumber + "." + t.getStatusIcon() + justTask); //print out the task that is done
+
+                list.set(taskNumber-1, (taskNumber)+ "." + t.getStatusIcon() + justTask);   //update the list
+            }
             else {
-                list.add(words);
+                list.add(i+ "." + t.getStatusIcon()+ words);
                 System.out.println("added: " + words);
+                i++;
             }
         }
     }
